b1ad7964d607d6f3ab4bda20510ca622e902300f
PhilippRosie <122967743+PhilippRosie@users.noreply.github.com>
2023-06-06 00:37:17 +0200

    deploying

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
     // COMMUNICATE WITH THE API
     axios
-      .post("http://localhost:5555/chat", { prompt })
+      .post("https://www.askanything.rosie.zone/chat", { prompt })
       .then((res) => {
         setResponse(res.data);
         setLoading(false);
